b532a0f96ebb9e018098ff681ba384f07a4db3e0
Neptune <nefo_1314@yahoo.com>
2022-11-01 21:37:11 +0800

    Өөрчлөв.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'user_type' => "simple_user",
+            // 'user_type' => "simple_user",
+            'user_type' => fake()->randomElement(['simple_user', 'employer']),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
